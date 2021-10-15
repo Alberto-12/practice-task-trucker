ff40c2a726e52eff89dfc4992e8ec55e289f90cc
Alberto Serafini <ilgumo@gmail.com>
2021-10-14 20:30:46 -0500

    Task trucker

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,35 @@
-import PropTypes from 'prop-types'
-import Button from './Button'
- 
- const Header = ({ title }) => {
-     const onClick = () => {
-         console.log('Click')
-     }
-     
-    return (
-        <header class='header'>
-           <h1 >{title}</h1>
-           <Button color='green' text='Add' onClick={onClick} />
-        </header>
-    )
-}
+import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
+import Button from "./Button";
+
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
+
+  return (
+    <header class="header">
+      <h1>{title}</h1>
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onAdd}
+        />
+      )}
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title: 'Task tracker',
-}
+  title: "Task tracker",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 //CSS in JS
 //const headingStyle = {
-    //color: 'red',
-    //background: 'black'
+//color: 'red',
+//background: 'black'
 //}
 
-export default Header
+export default Header;
